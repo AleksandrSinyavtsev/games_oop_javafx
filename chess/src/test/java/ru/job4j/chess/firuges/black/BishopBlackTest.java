c34afdedd6bb859a69a914b1990b86af9f76945c
Aleksandr Sinyavtsev <sinyavtsev.alek@yandex.ru>
2020-06-05 00:47:17 +0300

    Каркас шахматной доски[792#121674]

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -1,7 +1,15 @@
 package ru.job4j.chess.firuges.black;
 
+import org.junit.Test;
+import ru.job4j.chess.firuges.Cell;
+
 import static org.junit.Assert.*;
 
 public class BishopBlackTest {
 
+    @Test
+    public void blackBishopPositionTest() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        assertEquals(bishopBlack.position(), (Cell.C8));
+    }
 }
