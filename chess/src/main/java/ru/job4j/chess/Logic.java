323cbb59bbdc0b4d841df7a00327032162926f59
Aleksandr Sinyavtsev <sinyavtsev.alek@yandex.ru>
2020-06-09 22:20:42 +0300

    Каркас шахматной доски[792#121674]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -24,12 +24,17 @@ public class Logic {
     public boolean move(Cell source, Cell dest) {
         boolean rst = false;
         int index = this.findBy(source);
-        if (index != -1) {
-            Cell[] steps = this.figures[index].way(source, dest);
-            if (steps.length > 0 && steps[steps.length - 1].equals(dest)) {
-                rst = true;
-                this.figures[index] = this.figures[index].copy(dest);
+        try {
+            if (index != -1) {
+                Cell[] steps = this.figures[index].way(source, dest);
+                if (steps.length > 0 && steps[steps.length - 1].equals(dest)) {
+                    rst = true;
+                    this.figures[index] = this.figures[index].copy(dest);
+                }
             }
+        }
+        catch (IllegalStateException ex){
+            System.out.println(String.format("Could not way by diagonal from %s to %s", source, dest));
         }
         return rst;
     }
