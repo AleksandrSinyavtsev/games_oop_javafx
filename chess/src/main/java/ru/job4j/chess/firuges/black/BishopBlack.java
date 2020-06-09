0d9fe13eef5413152c14ae869456deeebdf8db43
Aleksandr Sinyavtsev <sinyavtsev.alek@yandex.ru>
2020-06-09 23:38:20 +0300

    Каркас шахматной доски[792#121674]

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -3,6 +3,8 @@ package ru.job4j.chess.firuges.black;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 
+import java.util.Arrays;
+
 public class BishopBlack implements Figure {
     private final Cell position;
 
@@ -16,14 +18,24 @@ public class BishopBlack implements Figure {
     }
 
     @Override
-    public Cell[] way(Cell source, Cell dest) {
-        throw new IllegalStateException(
-                String.format("Could not way by diagonal from %s to %s", source, dest)
-        );
+    public Cell[] way(Cell source, Cell dest) throws IllegalStateException {
+        if(!isDiagonal(source, dest)) {
+            throw new IllegalStateException(
+                    String.format("Could not way by diagonal from %s to %s", source, dest));
+        }
+        Cell[] cells = new Cell[8];
+        if(source.x < dest.x || source.y < dest.y) {
+
+        }
+        return System.arraycopy(cells, 0, new Cell[i], i);
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        return false;
+        boolean rsl = false;
+        if(source.x - dest.x == source.y - dest.y || source.x - dest.x == -(source.y - dest.y)) {
+            rsl = true;
+        }
+        return rsl;
     }
 
     @Override
