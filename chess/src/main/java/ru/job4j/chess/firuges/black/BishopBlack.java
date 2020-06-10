1de7e0d2bd6a4689a7dc4299a3adfdc60a39e450
Aleksandr Sinyavtsev <sinyavtsev.alek@yandex.ru>
2020-06-10 19:37:17 +0300

    Каркас шахматной доски[792#121674]

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -23,11 +23,15 @@ public class BishopBlack implements Figure {
             throw new IllegalStateException(
                     String.format("Could not way by diagonal from %s to %s", source, dest));
         }
-        Cell[] cells = new Cell[8];
+        Cell[] cells = new Cell[7];
+        int i = 0;
         if(source.x < dest.x || source.y < dest.y) {
-
+            while(source.x < dest.x) {
+                cells[i] = Cell.findBy(source.x + 1, source.y + 1);
+                i++;
+            }
         }
-        return System.arraycopy(cells, 0, new Cell[i], i);
+        return cells;
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
